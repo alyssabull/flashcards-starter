4023892dea93ff30c175d049cfb4a6721834da99
Alyssa Bull <alyssabull@gmail.com>
2020-10-07 18:05:52 -0600

    Should be able to create cards in the game file

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,24 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Turn = require('../src/Turn');
 
 class Game {
-  constructor(cardObj) {
+  constructor() {
+    this.deck = this.start();
+    this.currentRound = new Round(this.deck);
+  }
 
+  start() {
+    const deck = prototypeQuestions.map(function(card) {
+      const flashCard = new Card (card.id, card.question, card.answers, card.correctAnswer);
+      return flashCard;
+    });
+
+    return deck;
   }
 
   printMessage(deck, round) {
--- test/Game-test.js
+++ test/Game-test.js
@@ -2,17 +2,44 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Turn = require('../src/Turn');
 
 describe('Game', function() {
+  let game;
+  let deck;
+  let round;
+  let card1;
+  let card2;
+  let card3;
+
+  beforeEach(function() {
+    deck = new Deck([card1, card2, card3]);
+    game = new Game();
+    round = new Round(deck);
+    card1 = new Card(1, 'What is Harry Potter\'s patronus?', ['horse', 'otter', 'stag'], 'stag');
+    card2 = new Card(14, 'What does Dumbledore give Ron in his will?', ['deluminator', 'invisibility cloak', 'wand'], 'deluminator');
+    card3 = new Card(12, 'What does the Imperius Curse do?', ['kills', 'controls', 'mimics'], 'controls');
+  })
+
 
   it('should be a function', function() {
-    const game = new Game();
     expect(Game).to.be.a('function');
   });
 
   it('should be an instance of Game', function() {
-    const game = new Game();
     expect(game).to.be.an.instanceof(Game);
+  });
+
+  it('should keep track of the current round', function() {
+    expect(game.currentRound).to.be.an.instanceof(Round);
+  });
+
+  it('should be able to create cards', function() {
+    game.start();
+    expect(game.deck).to.be.an('array');
   })
 
 });
